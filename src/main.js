e12e1478d4da207656b74ff946f8339c6b5eb477
pablo44 <pawelrz@wp.pl>
2025-02-05 09:23:17 +0100

    rendere set up

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
 import './style.css'
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+//scene set up
+
+const scene = new THREE.Scene();
+
+//create pespective Camera
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+
+);
+
+//renderer as a DOM element
+
+const renderer= new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
